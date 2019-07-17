e9ba55f46cd86f007237b4e2ffbf92a921d85b54
lidongqi <254750646@qq.com>
2019-07-17 20:01:19 +0800

    ts

--- src/containers/Mime/index.tsx
+++ src/containers/Mime/index.tsx
@@ -2,10 +2,29 @@ import * as React from "react";
 import { connect } from "react-redux";
 import "./index.less";
 
-interface Props {}
-class Mime extends React.Component {
+interface Props {
+
+}
+interface State{
+    data:string,
+    list:number[]
+}
+class Mime extends React.Component<Props,State> {
+    state={
+        data:"123",
+        list:[1,2,3,4,5]
+    }
+    showIndex = (item:number):any=>{
+        console.log(item)
+       return item;
+    }
   render() {
-    return <div>Mime</div>;
+      return (<ul id="ul">
+          {this.state.list.map((item:number,index:number):any => {
+              return <li onClick={this.showIndex.bind(this,item)} key={index}>{item}</li>
+          })}
+
+      </ul>);
   }
 }
 export default connect()(Mime);
